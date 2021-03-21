292d28665d22dd34344ea32c4eafb66279c25e66
SukritJaAIproject <6170306321@student.chula.ac.th>
2021-03-21 16:24:55 +0700

    u

--- Telemedicine/02_SmoothStream_simple/Viewer.py
+++ Telemedicine/02_SmoothStream_simple/Viewer.py
@@ -2,18 +2,11 @@ import cv2
 import zmq
 import base64
 import numpy as np
-from datetime import datetime
-import os
 
 context = zmq.Context()
 footage_socket = context.socket(zmq.SUB)
 footage_socket.bind('tcp://*:5555')
 footage_socket.setsockopt_string(zmq.SUBSCRIBE, np.unicode(''))
-
-path = 'vdo'
-filename = str(datetime.now().strftime("%Y_%m_%d_%H_%M_%S")) + ".avi"
-fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
-videoWriter = cv2.VideoWriter(os.path.join(path, filename), fourcc, 30.0, (640, 480))
 
 while True:
     try:
@@ -21,8 +14,6 @@ while True:
         img = base64.b64decode(frame)
         npimg = np.fromstring(img, dtype=np.uint8)
         source = cv2.imdecode(npimg, 1)
-        print(source.shape)
-        videoWriter.write(source)
         cv2.imshow("Stream", source)
         cv2.waitKey(1)
 
