059322defded01c5c0b5490952fb95732596107e
SukritJaAIproject <6170306321@student.chula.ac.th>
2021-03-22 20:45:13 +0700

    Update 06_sound_recording_pyaudio.py

--- Webcam/06_sound_recording_pyaudio.py
+++ Webcam/06_sound_recording_pyaudio.py
@@ -1,43 +1,10 @@
-####################### sound recorder : pyaudio ######################################
-import pyaudio
-import wave
+###################### sound recorder : python-sounddevice######################################
+import sounddevice as sd
+from scipy.io.wavfile import write
 
-chunk = 1024  # Record in chunks of 1024 samples
-sample_format = pyaudio.paInt16  # 16 bits per sample
-channels = 2
-fs = 44100  # Record at 44100 samples per second
-seconds = 60*5 # time to record
-filename = "pyaudio.wav"
+fs = 44100  # Sample rate
+seconds = 60  # Duration of recording
 
-p = pyaudio.PyAudio()  # Create an interface to PortAudio
-
-print('Recording')
-
-stream = p.open(format=sample_format,
-                channels=channels,
-                rate=fs,
-                frames_per_buffer=chunk,
-                input=True)
-
-frames = []  # Initialize array to store frames
-
-# Store data in chunks for 3 seconds
-for i in range(0, int(fs / chunk * seconds)):
-    data = stream.read(chunk)
-    frames.append(data)
-
-# Stop and close the stream
-stream.stop_stream()
-stream.close()
-# Terminate the PortAudio interface
-p.terminate()
-
-print('Finished recording')
-
-# Save the recorded data as a WAV file
-wf = wave.open(filename, 'wb')
-wf.setnchannels(channels)
-wf.setsampwidth(p.get_sample_size(sample_format))
-wf.setframerate(fs)
-wf.writeframes(b''.join(frames))
-wf.close()
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+sd.wait()  # Wait until recording is finished
+write('output.wav', fs, myrecording)  # Save as WAV file
