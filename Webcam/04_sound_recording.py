304ce39706680b28ebf84ef477033ec4de7121d5
SukritJaAIproject <6170306321@student.chula.ac.th>
2021-03-22 20:32:50 +0700

    Update 04_sound_recording.py

--- Webcam/04_sound_recording.py
+++ Webcam/04_sound_recording.py
@@ -1,104 +1,20 @@
-####################### test webcam ######################################
-
-# import cv2
-# cap = cv2.VideoCapture(0)
-# while True:
-#    _, frame = cap.read()
-#    cv2.imshow('frame', frame)
-#    cv2.waitKey(1)
-
-
-####################### sound recorder : python-sounddevice######################################
-# import sounddevice as sd
-# from scipy.io.wavfile import write
-#
-# fs = 44100  # Sample rate
-# seconds = 60  # Duration of recording
-#
-# myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-# sd.wait()  # Wait until recording is finished
-# write('output.wav', fs, myrecording)  # Save as WAV file
-
-####################### sound recorder : pyaudio ######################################
-# import pyaudio
-# import wave
-#
-# chunk = 1024  # Record in chunks of 1024 samples
-# sample_format = pyaudio.paInt16  # 16 bits per sample
-# channels = 2
-# fs = 44100  # Record at 44100 samples per second
-# seconds = 10 # time to record
-# filename = "pyaudio.wav"
-#
-# p = pyaudio.PyAudio()  # Create an interface to PortAudio
-#
-# print('Recording')
-#
-# stream = p.open(format=sample_format,
-#                 channels=channels,
-#                 rate=fs,
-#                 frames_per_buffer=chunk,
-#                 input=True)
-#
-# frames = []  # Initialize array to store frames
-#
-# # Store data in chunks for 3 seconds
-# for i in range(0, int(fs / chunk * seconds)):
-#     data = stream.read(chunk)
-#     frames.append(data)
-#
-# # Stop and close the stream
-# stream.stop_stream()
-# stream.close()
-# # Terminate the PortAudio interface
-# p.terminate()
-#
-# print('Finished recording')
-#
-# # Save the recorded data as a WAV file
-# wf = wave.open(filename, 'wb')
-# wf.setnchannels(channels)
-# wf.setsampwidth(p.get_sample_size(sample_format))
-# wf.setframerate(fs)
-# wf.writeframes(b''.join(frames))
-# wf.close()
-
-
-####################### Saving and Converting Audio ######################################
-# import soundfile as sf
-#
-# # Extract audio data and sampling rate from file
-# data, fs = sf.read('pyaudio.wav')
-# # Save as FLAC file at correct sampling rate
-# sf.write('myfile.flac', data, fs)
-
-
-####################### Saving and Converting Audio ######################################
-# from pydub import AudioSegment
-# sound = AudioSegment.from_wav('pyaudio.wav')
-#
-# sound.export('pyaudio.mp3', format='mp3')
-
-
 ####################### Record Audio in Python ######################################
 from sys import byteorder
 from array import array
 from struct import pack
-
 import pyaudio
 import wave
 
 THRESHOLD = 500
-CHUNK_SIZE = 1024
+CHUNK_SIZE = 1024*10
 FORMAT = pyaudio.paInt16
 RATE = 44100
 
 def is_silent(snd_data):
-    "Returns 'True' if below the 'silent' threshold"
     return max(snd_data) < THRESHOLD
 
 def normalize(snd_data):
-    "Average the volume out"
+    # MAXIMUM = 16384
     MAXIMUM = 16384
     times = float(MAXIMUM)/max(abs(i) for i in snd_data)
 
@@ -140,27 +56,15 @@ def add_silence(snd_data, seconds):
     return r
 
 def record():
-    """
-    Record a word or words from the microphone and
-    return the data as an array of signed shorts.
 
-    Normalizes the audio, trims silence from the
-    start and end, and pads with 0.5 seconds of
-    blank sound to make sure VLC et al can play
-    it without getting chopped off.
-    """
     p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT, channels=1, rate=RATE,
-        input=True, output=True,
-        frames_per_buffer=CHUNK_SIZE)
+    stream = p.open(format=FORMAT, channels=1, rate=RATE, input=True, output=True, frames_per_buffer=CHUNK_SIZE)
 
     num_silent = 0
     snd_started = False
-
     r = array('h')
 
     while 1:
-        # little endian, signed short
         snd_data = array('h', stream.read(CHUNK_SIZE))
         if byteorder == 'big':
             snd_data.byteswap()
