06ee0ed489b41327a52c9b0b5cfa644e81d94f22
SukritJaAIproject <6170306321@student.chula.ac.th>
2021-03-23 15:09:13 +0700

    Update client_rev1.py

--- Tele_system/03_Socket_OpenCv_webcam_video_transmit_receive_wifi/client_rev1.py
+++ Tele_system/03_Socket_OpenCv_webcam_video_transmit_receive_wifi/client_rev1.py
@@ -3,7 +3,7 @@ import socket,cv2, pickle,struct
 
 # create socket
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_ip = '192.168.1.20' # paste your server ip address here
+host_ip = '192.168.1.34' # paste your server ip address here
 port = 9999
 client_socket.connect((host_ip,port)) # a tuple
 data = b""
