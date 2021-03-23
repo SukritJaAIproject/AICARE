539d997ff4de83cadc6bfc0f707a35132a50176d
SukritJaAIproject <6170306321@student.chula.ac.th>
2021-03-23 15:05:52 +0700

    Update Streamer_rev1.py

--- Tele_system/01_SmoothStream/Streamer_rev1.py
+++ Tele_system/01_SmoothStream/Streamer_rev1.py
@@ -1,53 +1,83 @@
 import argparse
+
 import cv2
-import numpy as np
 import zmq
-from constants import PORT
-from utils import string_to_image
 
-class StreamViewer:
-   def __init__(self, port=PORT):
-       context = zmq.Context()
-       self.footage_socket = context.socket(zmq.SUB)
-       self.footage_socket.bind('tcp://*:' + port)
-       self.footage_socket.setsockopt_string(zmq.SUBSCRIBE, np.unicode(''))
-       self.current_frame = None
-       self.keep_running = True
+from camera.Camera import Camera
+from constants import PORT, SERVER_ADDRESS
+from utils import image_to_string
 
-   def receive_stream(self, display=True):
-       self.keep_running = True
-       while self.footage_socket and self.keep_running:
-           try:
-               frame = self.footage_socket.recv_string()
-               self.current_frame = string_to_image(frame)
 
-               if display:
-                   cv2.imshow("Stream", self.current_frame)
-                   cv2.waitKey(1)
+class Streamer:
 
-           except KeyboardInterrupt:
-               cv2.destroyAllWindows()
-               break
-       print("Streaming Stopped!")
+    def __init__(self, server_address=SERVER_ADDRESS, port=PORT):
+        """
+        Tries to connect to the StreamViewer with supplied server_address and creates a socket for future use.
 
-   def stop(self):
-       self.keep_running = False
+        :param server_address: Address of the computer on which the StreamViewer is running, default is `localhost`
+        :param port: Port which will be used for sending the stream
+        """
+
+        print("Connecting to ", server_address, "at", port)
+        context = zmq.Context()
+        self.footage_socket = context.socket(zmq.PUB)
+        self.footage_socket.connect('tcp://' + server_address + ':' + port)
+        self.keep_running = True
+
+    def start(self):
+        """
+        Starts sending the stream to the Viewer.
+        Creates a camera, takes a image frame converts the frame to string and sends the string across the network
+        :return: None
+        """
+        print("Streaming Started...")
+        camera = Camera()
+        camera.start_capture()
+        self.keep_running = True
+
+        while self.footage_socket and self.keep_running:
+            try:
+                frame = camera.current_frame.read()  # grab the current frame
+                image_as_string = image_to_string(frame)
+                self.footage_socket.send(image_as_string)
+
+            except KeyboardInterrupt:
+                cv2.destroyAllWindows()
+                break
+        print("Streaming Stopped!")
+        cv2.destroyAllWindows()
+
+    def stop(self):
+        """
+        Sets 'keep_running' to False to stop the running loop if running.
+        :return: None
+        """
+        self.keep_running = False
+
 
 def main():
-   port = PORT
+    port = PORT
+    server_address = SERVER_ADDRESS
 
-   parser = argparse.ArgumentParser()
-   parser.add_argument('-p', '--port',
-                       help='The port which you want the Streaming Viewer to use, default'
-                            ' is ' + PORT, required=False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--server',
+                        help='IP Address of the server which you want to connect to, default'
+                             ' is ' + SERVER_ADDRESS,
+                        required=True)
+    parser.add_argument('-p', '--port',
+                        help='The port which you want the Streaming Server to use, default'
+                             ' is ' + PORT, required=False)
 
-   args = parser.parse_args()
-   if args.port:
-       port = args.port
+    args = parser.parse_args()
 
-   stream_viewer = StreamViewer(port)
-   stream_viewer.receive_stream()
+    if args.port:
+        port = args.port
+    if args.server:
+        server_address = args.server
+
+    streamer = Streamer(server_address, port)
+    streamer.start()
 
 
 if __name__ == '__main__':
-   main()
+    main()
