4021403f721c693c44a799a77fd35d82c4f5db34
Sukrit Jaidee <45908411+SukritJaAIproject@users.noreply.github.com>
2021-06-25 14:21:41 +0700

    Update change_tif_tiff.py

--- test_youtube/change_tif_tiff.py
+++ test_youtube/change_tif_tiff.py
@@ -1,3 +1,5 @@
+https://www.freecodecamp.org/news/how-to-build-an-image-type-convertor-in-six-lines-of-python-d63c3c33d1db/
+
 import os
 folder_path = ('/content/drive/MyDrive/AIHealthcare/AIcare_Phrase1/data/trainset/surprise/')
 test = os.listdir(folder_path)
@@ -10,3 +12,5 @@ test = os.listdir(folder_path)
 for images in tqdm(test):
     if images.endswith(".tiff"):
         os.remove(os.path.join(folder_path, images))
+        
+        
