95283e007c8ea49276d442af66e6205b3dc4cf87
Sukrit Jaidee <45908411+SukritJaAIproject@users.noreply.github.com>
2021-06-26 12:49:45 +0700

    upload

---
+++ test_youtube/annotation.py
@@ -0,0 +1,3 @@
+rasnetmark = {'anger':0, 'disgust':1,'fear':2,'happiness':3,'sadness':4,'surprise':5,'neutral':6}
+rasnetmark = {0;'anger', 1:'disgust', 2:'fear', 3:'happiness', 4:'sadness', 5:'surprise', 6:'neutral'}
+vgg
