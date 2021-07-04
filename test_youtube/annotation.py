4d2d14dfae081afcbca5a9bfe55a1d8f959a9302
Sukrit Jaidee <45908411+SukritJaAIproject@users.noreply.github.com>
2021-07-04 19:39:33 +0700

    upload

--- test_youtube/annotation.py
+++ test_youtube/annotation.py
@@ -3,5 +3,4 @@ rasnetmark = {0;'anger', 1:'disgust', 2:'fear', 3:'happiness', 4:'sadness', 5:'s
 model_FERG_DB_256 = {0: 'angry', 1: 'happy', 2: 'neutral', 3: 'sad'}
 model_ck_kaggle_jaffe = {0: 'angry', 1: 'happy', 2: 'neutral', 3: 'sad'}
 FAUS_CK = {0: 'AU1',1: 'AU12',2: 'AU14',3: 'AU15',4: 'AU17',5: 'AU2',6: 'AU20',7: 'AU23',8: 'AU24',9: 'AU25',10: 'AU26',11: 'AU27',12: 'AU4',13: 'AU5',14: 'AU6',15: 'AU7',16: 'AU9'}
-label_ck+ = (1="anger", 3="disgust", 4="fear", 5="happy", 6="sadness", 7="surprise")
-emotions = ["neutral", "angry", "contempt", "disgust", "fear", "happy", "sad", "surprise"]
+label_ckplus = {0:"neutral", 1: "angry", 2:"contempt", 3:"disgust", 4:"fear", 5:"happy", 6:"sad", 7:"surprise"}
